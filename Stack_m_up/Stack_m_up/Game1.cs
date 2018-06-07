dcec55a1f9beafd96d2fc617f5cd91b9f09491c9
Jahdah <Jahdah-thelen@live.de>
2018-06-07 16:16:09 +0200

    Splitscreen added

--- Stack_m_up/Stack_m_up/Game1.cs
+++ Stack_m_up/Stack_m_up/Game1.cs
@@ -10,68 +10,44 @@ namespace Stack_m_up
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        GameManager game;
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            game = new GameManager( 4 );
         }
-
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
+        
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
+            game.Initialize();
 
             base.Initialize();
         }
-
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
+        
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            game.LoadContent( Content );
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
         protected override void UnloadContent()
         {
-            // TODO: Unload any non ContentManager content here
         }
-
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        
         protected override void Update(GameTime gameTime)
         {
-            // TODO: Add your update logic here
+            game.Update(gameTime);
 
             base.Update(gameTime);
         }
-
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
+            game.Draw(gameTime, graphics, spriteBatch);
 
             base.Draw(gameTime);
         }
---
+++ Stack_m_up/Stack_m_up/GameManager.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections;
+
+namespace Stack_m_up
+{
+    class GameManager
+    {
+        ArrayList playfields;
+        int playFieldAmount;
+
+        double windowHeight, windowWidth;
+        Texture2D image;
+
+        public GameManager( int playFieldAmount )
+        {
+            this.playFieldAmount = playFieldAmount;
+            playfields = new ArrayList();
+        }
+        
+        public void Initialize()
+        {
+            var applicationView = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView();
+            windowHeight = applicationView.VisibleBounds.Height;
+            windowWidth = applicationView.VisibleBounds.Width;
+
+        }
+
+        public void LoadContent( ContentManager content )
+        {
+            createPlayFields(playFieldAmount);
+
+            image = content.Load<Texture2D>("logo");
+        }
+
+        public void UnloadContent()
+        {
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            var applicationView = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView();
+            if (applicationView.VisibleBounds.Height != windowHeight || applicationView.VisibleBounds.Height != windowWidth)
+            {
+                windowHeight = applicationView.VisibleBounds.Height;
+                windowWidth = applicationView.VisibleBounds.Width;
+
+                createPlayFields(playFieldAmount);
+            }
+        }
+
+        public void Draw(GameTime gameTime, GraphicsDeviceManager graphics, SpriteBatch spriteBatch)
+        {
+            Viewport original = graphics.GraphicsDevice.Viewport;
+
+            foreach (Viewport view in playfields)
+            {
+                graphics.GraphicsDevice.Viewport = view;
+                spriteBatch.Begin();
+                spriteBatch.Draw(image, new Vector2(0, 200), Color.AliceBlue);
+                spriteBatch.End();
+            }
+
+            graphics.GraphicsDevice.Viewport = original;
+        }
+
+        private void createPlayFields( int amount )
+        {
+            playfields.Clear();
+
+            for (int i = 0; i < amount; ++i)
+            {
+                Viewport view = new Viewport();
+                view.X = Convert.ToInt32((windowWidth / amount) * i);
+                view.Y = 0;
+                view.Width = Convert.ToInt32(windowWidth / amount);
+                view.Height = Convert.ToInt32(windowHeight);
+                view.MinDepth = 0;
+                view.MaxDepth = 1;
+
+                playfields.Add(view);
+            }
+        }
+
+    }
+}
